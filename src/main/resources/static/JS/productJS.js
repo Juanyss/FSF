953122e1c6b7c7083895b60b039b081e50c1f096
Juanyss <Juanyss88@gmail.com>
2019-04-12 19:41:31 -0300

    Home (html, css) & Product (Html, css, js) added

--- src/main/resources/static/JS/productJS.js
+++ src/main/resources/static/JS/productJS.js
@@ -1,28 +1,59 @@
 $(document).ready(function () {
         $.ajax({
-            url: "/api/products/",
+            url: "/api/products",
             type: "GET",
             success: function (result) {
-
-                $("#productDiv").empty();
-                $("#productDiv").append("<h3>Lista de productos</h3>");
+                $("#productList").empty();
                 for (x = 0; x < result.length; x++) {
-                    $("#productDiv").append(
-                        "<fieldset class='field_set'>" +
-                        "<div><strong><label>Nombre: </label></strong><label type='text' >"+ result[x].name +"</label></div>" +
-                        "<div><strong><label>Tipo: </label></strong><label type='text' >"+ result[x].type +"</label></div>" +
-                        "<div><strong><label>Detalle: </label></strong><label type='text' >"+ result[x].detail +"</label></div>" +
-                        "<div><strong><label>Stock: </label></strong><label type='text' >"+ result[x].stock.toString() +"</label></div>" +
-                        "<div><strong><label>Costo: </label></strong><label type='text' >"+ result[x].cost.toString() +"</label></div>" +
-                        "<div><strong><label>Distribuidor: </label></strong><label type='text' >"+ result[x].distributor +"</label></div>" +
-                        "<div><strong><label>Precio: </label></strong><label type='text' >"+ result[x].price.toString() +"</label></div>" +
-                        "</fieldset>"
+                    $("#productList").append(
+                        "<table>" +
+                        "<tbody>" +
+                        "<td class='column1'>"+ result[x].name +"</td>" +
+                        "<td class='column2'>"+ result[x].distributor +"</td>" +
+                        "<td class='column3'>"+ result[x].detail +"</td>" +
+                        "<td class='column4'>"+ result[x].stock.toString() +"</td>" +
+                        "<td class='column5'>$"+ result[x].cost.toString() +"</td>" +
+                        "<td class='column5'>$"+ result[x].price.toString() +"</td>" +
+                        "</tbody>" +
+                        "</table>" +
+                        "</div>"
                     )
                 }
                 ;
             }
-        })
+        });
+
+    $.ajax({
+        url: "/api/distributors",
+        type: "GET",
+        success: function (result) {
+            $("#distributorDropDown").empty();
+            for (x = 0; x < result.length; x++) {
+                $("#distributorDropDown").append(
+                "<option  value='"+ result[x].idDistributor +"'>"+ result[x].brand.toString() +"</option>"
+                )
+            }
+            ;
+        }
+    });
+
+
+});
+
+
+$("#addProduct").click(function(){
+    $("#productForm").fadeToggle();
 })
+
+$(document).mouseup(function (e) {
+    var container = $("#productForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+});
 
 
 
